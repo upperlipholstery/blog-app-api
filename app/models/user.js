387c8606323aaf511cb3a455ed789359dfe74bca
taharon <taharon1990@gmail.com>
2020-05-03 20:28:54 -0400

    Like routes
    Can like tomes now, and remove likes
    Co-Author: @ArKaneVision

--- app/models/user.js
+++ app/models/user.js
@@ -14,6 +14,9 @@ const userSchema = new mongoose.Schema({
   imageUrl: String,
   imageTitle: String,
   tomes: [tome.schema],
+  likedTomes: {
+    type: Array
+  },
   favTomes: {
     type: Array
   },
